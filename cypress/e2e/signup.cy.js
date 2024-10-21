c79de35c811d4f3b4402a971fbe8322d401f3c9c
Trey Turner <treyturner@users.noreply.github.com>
2024-10-21 13:28:18 -0500

    test: create signup password input validation test

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,25 +1,28 @@
-describe('Signup', () => {
+describe("Signup", () => {
   beforeEach(() => {
-    cy.visit('signup.html');
+    cy.visit("signup.html");
   });
 
-  it('submits the form without errors', () => {
-    cy.findByLabelText('Username').type('sparkbox');
-    cy.findByLabelText('Password').type('passwords-are-actually-really-cool');
-    cy.findByRole('button', { text: 'Submit' }).click();
-    cy.get('.error').should('not.exist');
-    cy.get('.error_message').should('not.exist');
+  it("submits the form without errors", () => {
+    cy.findByLabelText("Username").type("sparkbox");
+    cy.findByLabelText("Password").type("passwords-are-actually-really-cool");
+    cy.findByRole("button", { text: "Submit" }).click();
+    cy.get(".error").should("not.exist");
+    cy.get(".error_message").should("not.exist");
   });
 
-  describe('form validation displays an error when', () => {
-    it('the username is empty', () => {
-      cy.findByRole('button', { text: 'Submit' }).click();
-      cy.findByText('enter a username').should('have.class', 'error_message');
-      cy.get('input[name=username]').should('have.class', 'error');
+  describe("form validation displays an error when", () => {
+    it("the username is empty", () => {
+      cy.findByRole("button", { text: "Submit" }).click();
+      cy.findByText("enter a username").should("have.class", "error_message");
+      cy.get("input[name=username]").should("have.class", "error");
     });
 
-    xit('the password is less than 10 characters', () => {
-      // add test commands
+    it("the password is less than 10 characters", () => {
+      cy.findByLabelText("Username").type("sparkbox");
+      cy.findByRole("button", { text: "Submit" }).click();
+      cy.contains("password must").should("have.class", "error_message");
+      cy.get("input[name=password]").should("have.class", "error");
     });
   });
 });
